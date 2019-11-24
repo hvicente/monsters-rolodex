92cc292611c71b69c6356cec2bddab54ca7a595e
Helios Vicente <helios.vicente@gmail.com>
2019-11-24 22:01:21 +0100

    Updates

---
+++ precache-manifest.e59596f68807df8703db05f0f60e2193.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b63c815608d53971c0a4709e2bd29fd7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d3f409b7fb97e65ccccd",
+    "url": "/monsters-rolodex/static/css/main.d31d1aed.chunk.css"
+  },
+  {
+    "revision": "072144937a437c299fb0",
+    "url": "/monsters-rolodex/static/js/2.12dc0d94.chunk.js"
+  },
+  {
+    "revision": "d3f409b7fb97e65ccccd",
+    "url": "/monsters-rolodex/static/js/main.733d35bc.chunk.js"
+  },
+  {
+    "revision": "ec765bb5ff3b7357d49a",
+    "url": "/monsters-rolodex/static/js/runtime-main.db675a29.js"
+  }
+]);
